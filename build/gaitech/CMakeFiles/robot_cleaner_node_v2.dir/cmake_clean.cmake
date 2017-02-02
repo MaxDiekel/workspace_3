55c8338cb18d97160abb8923a8fa7b3d35b87d28
MaxDiekel <zdiekel@clemson.edu>
2017-02-01 22:04:59 -0500

    add build, devel, and src folders

---
+++ build/gaitech/CMakeFiles/robot_cleaner_node_v2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_cleaner_node_v2.dir/src/turtlesim/cleaning_app/robot_cleaner_v2.cpp.o"
+  "/home/max/gaitech_ws/devel/lib/gaitech/robot_cleaner_node_v2.pdb"
+  "/home/max/gaitech_ws/devel/lib/gaitech/robot_cleaner_node_v2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_cleaner_node_v2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/turtlesim_cleaner/CMakeFiles/robot_cleaner_node1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_cleaner_node1.dir/src/robot_cleaner1.cpp.o"
+  "/home/max/gaitech_ws/devel/lib/turtlesim_cleaner/robot_cleaner_node1.pdb"
+  "/home/max/gaitech_ws/devel/lib/turtlesim_cleaner/robot_cleaner_node1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_cleaner_node1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
